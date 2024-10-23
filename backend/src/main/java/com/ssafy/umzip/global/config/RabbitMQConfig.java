50aff1f205f61b9cbc1953f6727a3a50c66e3903
mins00oo <alstn7223@gmail.com>
2024-10-23 13:24:31 +0900

    [2024.10.23] RabbitMQConfig 파일 수정
    필요한 어노테이션 및 연결 객체 추가

--- backend/src/main/java/com/ssafy/umzip/global/config/RabbitMQConfig.java
+++ backend/src/main/java/com/ssafy/umzip/global/config/RabbitMQConfig.java
@@ -4,23 +4,57 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
+    private static final String CHAT_QUEUE_NAME = "chat.queue";
+    private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+    private static final String ROUTING_KEY = "room.*";  // 여러 채팅방 처리
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    // Queue 등록
     @Bean
-    public TopicExchange topicExchange() {
-        return new TopicExchange("chatroom-exchange");
+    public Queue queue() {
+        return new Queue(CHAT_QUEUE_NAME, true);  // durable 큐 생성
+    }
+
+    // Exchange 등록
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(CHAT_EXCHANGE_NAME, true, false);  // durable, auto-delete false
+    }
+
+    // Exchange와 Queue 바인딩
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);  // 라우팅 키를 통해 메시지 전달
     }
 
     @Bean
-    public Queue chatroomQueue() {
-        return new Queue("chatroom-queue");
-    }
-
-    @Bean
-    public Binding binding(TopicExchange topicExchange, Queue chatroomQueue) {
-        return BindingBuilder.bind(chatroomQueue).to(topicExchange).with("chatroom.#");
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost(host);  // 호스트 설정
+        factory.setUsername(username);  // 사용자명
+        factory.setPassword(password);  // 비밀번호
+        factory.setPort(port);  // 포트
+        return factory;
     }
 }
--- backend/src/main/java/com/ssafy/umzip/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/umzip/global/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -35,6 +36,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
+        registry.setPathMatcher(new AntPathMatcher("."));
 
         registry.enableStompBrokerRelay("/exchange", "/queue")
                 .setRelayHost(host)
