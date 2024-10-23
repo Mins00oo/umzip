c9f719f9300d06b8dd843eaa7430b0a421a5e8aa
mins00oo <alstn7223@gmail.com>
2024-10-23 21:16:23 +0900

    [2024.10.23] merge 이전 WebSocketConfig 커밋

--- backend/src/main/java/com/ssafy/umzip/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/umzip/global/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -34,15 +35,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-
-        registry.enableStompBrokerRelay("/topic", "/queue", "/chatroom")
+        registry.enableStompBrokerRelay("/exchange")
                 .setRelayHost(host)
                 .setRelayPort(port)
                 .setClientLogin(username)
                 .setClientPasscode(password)
                 .setSystemLogin(username)
                 .setSystemPasscode(password);
+
+        //메시지 발행 url
+        registry.setPathMatcher(new AntPathMatcher("."));
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 
 }
